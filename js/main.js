18c1fecc8358ad7cf7548ca3df1e38638746fb8e
juhyejin <juhyejin.dev@gmail.com>
2023-03-06 18:01:17 +0900

    검색 레이아웃구현,기능구현

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니!
 const basketStaterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStaterEl.querySelector('.basket');
 
@@ -14,13 +15,29 @@ basketEl.addEventListener('click', function (event){
   event.stopPropagation();
 })
 
-window.addEventListener('click', function (){
-  hideBasket();
-})
+window.addEventListener('click', hideBasket)
 
 function showBasket(){
   basketEl.classList.add('show');
 }
 function hideBasket(){
   basketEl.classList.remove('show');
+}
+
+//검색
+const headerEl = document.querySelector('header');
+const searchWraoEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = headerEl.querySelector('.search-closer')
+const searchShadowEl = headerEl.querySelector('.shadow')
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+function showSearch(){
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed')
+}
+function hideSearch(){
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed')
 }
